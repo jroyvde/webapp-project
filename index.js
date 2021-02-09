92deb56e6cfeac6d63d0f1a564806a89e1732ecb
gghigi <23176223+gghigi@users.noreply.github.com>
2021-02-10 10:26:22 +1100

    add index.js and query selectors

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//#ID names newVar
+const nameInput = document.querySelector('#name');
+console.log("name:  " + nameInput.value);
+
+const descriptionInput = document.querySelector('#description');
+console.log("description:  " + descriptionInput.value);
+
+const assignInput = document.querySelector('#assign');
+console.log("assign:  " + assignInput.value);
+
+const dueDateInput = document.querySelector('#dueDate');
+console.log("date:  " + dueDateInput.value);
+
+
+//function
+validFormFieldInputc = (data) => {
+
+}
+
