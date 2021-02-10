115a3a99b4f275c22de9c25603d1a51b477aa139
jroyvde <jacob.vde@protonmail.com>
2021-02-10 11:49:49 +1100

    complete step 2 of task 4, adding if statements to check form inputs

--- index.js
+++ index.js
@@ -12,9 +12,36 @@ console.log("assign:  " + assignInput.value);
 const dueDateInput = document.querySelector('#dueDate');
 console.log("date:  " + dueDateInput.value);
 
+const statusInput = document.querySelector("#status");
+console.log("status:  " + statusInput.value);
+
 
 //function
-validFormFieldInputc = (data) => {
+validFormFieldInput = (data) => {
 
 }
 
+// Check Name is longer than 5
+if (nameInput.value.length > 5) {
+    console.log('name longer')
+}
+
+// Check Description is longer than 5
+if (descriptionInput.value.length > 5) {
+    console.log("desc longer");
+}
+
+// Check Assigned To is longer than 5
+if (assignInput.value.length > 5) {
+    console.log("assigned longer");
+}
+
+// Check Due Date is not empty
+if (dueDateInput.value != "") {
+    console.log("date not empty");
+}
+
+// Check Status is not empty
+if (statusInput.value != "") {
+    console.log("status not empty");
+}
