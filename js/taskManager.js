60c3fce919b6436764129389c24f0e95afaeea6c
jroyvde <jacob.vde@protonmail.com>
2021-02-12 10:31:50 +1100

    Integrate task object IDs into HTML via template literal

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,33 +1,29 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
   const html = `
-  <li>     
+  <li data-task-id="${id}">     
     <div class="row m-2">
-        <!--This column contains the CARD for the sample task-->
-        <div class="col-10">
-          <div class="card-header" style="text-align:left;">
-            ${name}
+      <!--This column contains the CARD for the sample task-->
+      <div class="col-12">
+        <div class="card-header" style="text-align:left;">
+          ${name}
+        </div>
+        <div class="card-body" style="text-align: left;">
+          <p class="card-text ">${description}</p>
+        </div>
+        <div class="card-footer text-muted row px-0 mx-0">
+          <div class="col-md-7 d-flex align-items-center">
+            <span class="mr-4">Assigned to: ${assignedTo}</span>
+            <span class="mr-4">Date: ${dueDate}</span>
+            <span class="">Status: ${status}</span>
           </div>
-          <div class="card-body" style="text-align: left;">
-            <p class="card-text ">${description}</p>
-          </div>
-          <div class="card-footer text-muted row px-0 mx-0">
-            <div class="col-md-7 d-flex align-items-center">
-              <span class="mr-4">Assigned to: ${assignedTo}</span>
-              <span class="mr-4">Date: ${dueDate}</span>
-              <span class="">Status: ${status}</span>
-            </div>
-            <div class="col-md-5 d-flex justify-content-end">
-              <a href="#" class="btn btn-success done-button">Done</a>
-              <a href="#" class="btn btn-danger ml-1">Delete</a>
-            </div>
+          <div class="col-md-5 d-flex justify-content-end">
+            <a href="#" class="btn btn-success done-button">Done</a>
+            <a href="#" class="btn btn-danger ml-1">Delete</a>
           </div>
         </div>
-        <!--This column contains the CHECKBOX for the sample task-->
-        <div class="col-2 p-1">
-          <input type="checkbox" class="checkbox">
-        </div>
+      </div>
     </div>
-  </li>`; ;
+  </li>`;
   return html;
 }
 
@@ -54,7 +50,7 @@ class TaskManager {
     this.tasks.forEach((element) => {
       const date = new Date(element.dueDate);
       const formattedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-      const taskHtml = createTaskHtml(element.name, element.description, element.assignedTo, formattedDate, element.status)
+      const taskHtml = createTaskHtml(element.name, element.description, element.assignedTo, formattedDate, element.status, element.id)
       tasksHtmlList.push(taskHtml);
     })
 
